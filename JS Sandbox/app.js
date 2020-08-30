244665e447aff4804f3ae4e2a7d5f44415d95ee4
Uday Kumar <udaykumaruking@gmail.com>
2020-08-30 17:28:05 +0530

    first commit

---
+++ JS Sandbox/app.js
@@ -0,0 +1,33 @@
+// var name = "John Doe";
+// console.log(name);
+// naem = "Steve Smith";
+// console.log(name);
+
+// var greeting;
+// console.log(greeting);
+// greeting = "Hello";
+// console.log(greeting);
+
+
+// var firstName = "John";//Camel Case
+// var first_name = "John";//UnderScore Case
+// var FirstName = "John"//Pascal Case
+// var firstname = "John"; //For god sake don't use this convention
+
+
+// var // Used to declare variables
+//     let // same to var, in this we can reassign variables
+// const // same to var, in this we can't reassign variables
+
+
+//     // Data Types
+//     String
+// Number
+// Boolean
+// null
+// undefined
+
+// Arrays
+// Objet Literals
+
+
