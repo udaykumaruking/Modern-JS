09c38b930bcff63491bedc54608536d57d34a654
Uday Kumar <udaykumaruking@gmail.com>
2020-08-31 21:55:33 +0530

    Added JS

---
+++ loan_calculator/app.js
@@ -0,0 +1,50 @@
+// Uday
+// Listen for Submit
+document
+  .getElementById('loan-form')
+  .addEventListener('submit', calculateResult);
+
+//Calculate Result function
+function calculateResult(e) {
+  console.log('cal');
+  // UI Variables
+  const amount = document.getElementById('amount');
+  const interest = document.getElementById('interest');
+  const years = document.getElementById('years');
+  const monthlyPayment = document.getElementById('monthly-payment');
+  const totalPayment = document.getElementById('total-payment');
+  const totalInterest = document.getElementById('monthly-interest');
+
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
+  } else {
+    showError('Please Check your Numbers');
+  }
+
+  e.preventDefault();
+}
+
+function showError(error) {
+  const errorDiv = document.createElement('div');
+
+  const card = document.querySelector('.card');
+  const heading = document.querySelector('.heading');
+  errorDiv.className = 'alert alert-danger';
+  errorDiv.appendChild(document.createTextNode(error));
+  card.insertBefore(errorDiv, heading);
+
+  setTimeout(clearError, 3000);
+}
+
+function clearError() {
+  document.querySelector('.alert').remove();
+}
