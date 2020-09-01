1fd9efcc38b0650b6a443adbc758d57770ae5c24
Uday Kumar <udaykumaruking@gmail.com>
2020-09-01 15:27:31 +0530

    Loan Calculator advancements

--- scrap.js
+++ scrap.js
@@ -1,15 +1,15 @@
-// uday
 document.getElementById('loan-form').addEventListener('submit', function (e) {
   document.getElementById('results').style.display = 'none';
 
   document.getElementById('loading').style.display = 'block';
 
   setTimeout(calculateResults, 2000);
+
   e.preventDefault();
 });
 
 function calculateResults() {
-  console.log('cal');
+  console.log('Calculating...');
   const amount = document.getElementById('amount');
   const interest = document.getElementById('interest');
   const years = document.getElementById('years');
@@ -30,21 +30,27 @@ function calculateResults() {
     totalInterest.value = (monthly * calculatedPayments - principal).toFixed(2);
 
     document.getElementById('results').style.display = 'block';
+
     document.getElementById('loading').style.display = 'none';
   } else {
-    showError('Please Check your Numbers');
+    showError('Please check your numbers');
   }
 }
 
 function showError(error) {
   document.getElementById('results').style.display = 'none';
+
   document.getElementById('loading').style.display = 'none';
+
   const errorDiv = document.createElement('div');
 
   const card = document.querySelector('.card');
   const heading = document.querySelector('.heading');
+
   errorDiv.className = 'alert alert-danger';
+
   errorDiv.appendChild(document.createTextNode(error));
+
   card.insertBefore(errorDiv, heading);
 
   setTimeout(clearError, 3000);
