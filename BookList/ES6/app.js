7cd77c858f9f7f09e0a36ac23c6cce0b5cf06eb7
Uday Kumar <udaykumaruking@gmail.com>
2020-09-05 18:38:10 +0530

    added local storage

--- BookList/ES6/app.js
+++ BookList/ES6/app.js
@@ -41,6 +41,42 @@ class UI {
     document.getElementById('isbn').value = '';
   }
 }
+// Local storage
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
+    }
+    return books;
+  }
+  static displayBooks() {
+    const books = Store.getBooks();
+    books.forEach(function (book) {
+      const ui = new UI();
+      // Add book to UI
+      ui.addBookToList(book);
+    });
+  }
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+    books.forEach(function (book, index) {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+    localStorage.setItem('books', JSON.stringify(books));
+  }
+}
+//DOM load event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
 document.getElementById('book-form').addEventListener('submit', function (e) {
   //   console.log('test');
   // Getting Form Values
@@ -61,6 +97,7 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
     // Add book to list
 
     ui.addBookToList(book);
+    Store.addBook(book);
     ui.showAlert('Book Added Successfully', 'success');
 
     // clear fields
@@ -83,6 +120,7 @@ document.getElementById('book-list').addEventListener('click', function (e) {
   //   console.log(123);
   const ui = new UI();
   ui.deleteBook(e.target);
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
   // Show message
   ui.showAlert('Book Removed!', 'removed');
   e.preventDefault();
