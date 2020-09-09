4125a07ced85b4bbd33d6436cb4b7cd9a5f15793
Uday Kumar <udaykumaruking@gmail.com>
2020-09-09 17:21:13 +0530

    HTTP Library

---
+++ easyHTTP/app.js
@@ -0,0 +1,68 @@
+const http = new easyHTTP();
+
+// Get posts
+
+// http.get('https://jsonplaceholder.typicode.com/posts', function (err, posts) {
+//   //   console.log(posts);
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(posts);
+//   }
+// });
+
+//Get single post
+
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function (err, post) {
+//   //   console.log(posts);
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+//Creating DAta
+
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post',
+};
+
+// Creating post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function (
+//   err,
+//   post,
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+//Update Post
+
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function (
+//   err,
+//   post,
+// ) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// deletee posts
+
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function (
+  err,
+  response,
+) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(response);
+  }
+});
