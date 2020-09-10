36a48486c90f596f6cebaec70e5d15be5dd1858f
Uday Kumar <udaykumaruking@gmail.com>
2020-09-10 17:39:09 +0530

    HTTP v2

---
+++ easyHTTPv2/easyhttp.js
@@ -0,0 +1,55 @@
+class EasyHTTP {
+  get(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url)
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+  post(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+  put(url, data) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      })
+        .then(res => res.json())
+        .then(data => resolve(data))
+        .catch(err => reject(err));
+    });
+  }
+
+  delete(url) {
+    return new Promise((resolve, reject) => {
+      fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        // body: JSON.stringify(data),
+      })
+        .then(res => res.json())
+        .then(data => resolve('Resource Deleted'))
+        .catch(err => reject(err));
+    });
+  }
+}
