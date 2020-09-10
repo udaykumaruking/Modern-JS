521223f42696b134f6f09fa94169450f65714eb9
Uday Kumar <udaykumaruking@gmail.com>
2020-09-10 17:59:12 +0530

    v3 http

---
+++ easyHTTPv3/app.js
@@ -0,0 +1,27 @@
+const http = new EasyHTTP();
+
+// http
+//   .get('https://jsonplaceholder.typicode.com/users')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+const data = {
+  name: 'uday',
+  username: 'uking',
+  email: 'udaykumaruking@gmail.com',
+};
+
+http
+  .post('https://jsonplaceholder.typicode.com/users', data)
+  .then(data => console.log(data))
+  .catch(err => console.log(err));
+
+// http
+//   .put('https://jsonplaceholder.typicode.com/users/2', data)
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
+
+// http
+//   .put('https://jsonplaceholder.typicode.com/users/2')
+//   .then(data => console.log(data))
+//   .catch(err => console.log(err));
